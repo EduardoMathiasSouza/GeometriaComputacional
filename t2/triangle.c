62a860286fdd3641da9161a1ec31d72424f0b257
EduardoMathiasSouza <eduardomsouza@ufpr.br>
2023-05-18 15:34:01 -0300

    [ADD] v1 T2

---
+++ t2/triangle.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct point {
+    double x;
+    double y;
+};
+
+struct triangle {
+    struct point a;
+    struct point b;
+    struct point c;
+};
+
+int is_ear(struct point a, struct point b, struct point c, struct point *points, int num_points) {
+    int i;
+    double cross_product = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    if (cross_product <= 0) {
+        return 0; // not a convex corner
+    }
+    for (i = 0; i < num_points; i++) {
+        if ((points[i].x == a.x && points[i].y == a.y) || (points[i].x == b.x && points[i].y == b.y) || (points[i].x == c.x && points[i].y == c.y)) {
+            continue;
+        }
+        double cross_product1 = (b.x - a.x) * (points[i].y - a.y) - (b.y - a.y) * (points[i].x - a.x);
+        double cross_product2 = (c.x - b.x) * (points[i].y - b.y) - (c.y - b.y) * (points[i].x - b.x);
+        double cross_product3 = (a.x - c.x) * (points[i].y - c.y) - (a.y - c.y) * (points[i].x - c.x);
+        if (cross_product1 >= 0 && cross_product2 >= 0 && cross_product3 >= 0) {
+            return 0; // point inside triangle
+        }
+    }
+    return 1;
+}
+
+void triangulate(struct point *points, int num_points, struct triangle **triangles, int *num_triangles) {
+    int i, j, k;
+    int *indices = malloc(num_points * sizeof(int));
+    for (i = 0; i < num_points; i++) {
+        indices[i] = i;
+    }
+    *triangles = NULL;
+    *num_triangles = 0;
+    int orientation = 0; // 0 for counterclockwise, 1 for clockwise
+    double cross_product = 0;
+    for (i = 0; i < num_points; i++) {
+        j = (i + 1) % num_points;
+        k = (i + 2) % num_points;
+        cross_product = (points[j].x - points[i].x) * (points[k].y - points[i].y) - (points[j].y - points[i].y) * (points[k].x - points[i].x);
+        if (cross_product != 0) {
+            orientation = (cross_product > 0) ? 0 : 1;
+            break;
+        }
+    }
+    if (orientation == 1) {
+        for (i = 0; i < num_points; i++) {
+            j = (i + 1) % num_points;
+            indices[i] = (num_points - 1) - i;
+        }
+    }
+    while (num_points >= 3) {
+        for (i = 0; i < num_points; i++) {
+            j = (i + 1) % num_points;
+            k = (i + 2) % num_points;
+            if (is_ear(points[indices[i]], points[indices[j]], points[indices[k]], points, num_points)) {
+                struct triangle t = {points[indices[i]], points[indices[j]], points[indices[k]]};
+                (*triangles) = realloc(*triangles, (*num_triangles + 1) * sizeof(struct triangle));
+                (*triangles)[*num_triangles] = t;
+                (*num_triangles)++;
+                for (j = i + 1; j < num_points - 1; j++) {
+                    indices[j] = indices[j + 1];
+                }
+                num_points--;
+                break;
+            }
+        }
+    }
+    free(indices);
+}
+
+int main() {
+    struct point points[] = {{1, 4}, {1, 20}, {15, 20}, {15, 4}};
+    int num_points = sizeof(points) / sizeof(struct point);
+    struct triangle *triangles;
+    int num_triangles;
+    triangulate(points, num_points, &triangles, &num_triangles);
+    printf("%d triangles:\n", num_triangles);
+    int i;
+    for (i = 0; i < num_triangles; i++) {
+        printf("Triangle %d: (%f, %f), (%f, %f), (%f, %f)\n", i+1, triangles[i].a.x, triangles[i].a.y, triangles[i].b.x, triangles[i].b.y, triangles[i].c.x, triangles[i].c.y);
+    }
+    free(triangles);
+    return 0;
+}

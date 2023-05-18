25e80a2d39b8a57e0d19aa5daf4020db9c7e8623
EduardoMathiasSouza <eduardomsouza@ufpr.br>
2023-05-18 16:33:56 -0300

    Talvez agora esteja certo

--- t2/triangle.c
+++ t2/triangle.c
@@ -1,93 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct point {
-    double x;
-    double y;
-};
+typedef struct {
+    float x;
+    float y;
+} Point;
 
-struct triangle {
-    struct point a;
-    struct point b;
-    struct point c;
-};
+typedef struct {
+    int v1;
+    int v2;
+    int v3;
+} Triangle;
 
-int is_ear(struct point a, struct point b, struct point c, struct point *points, int num_points) {
+int isEarTip(Point* polygon, int numVertices, int v1, int v2, int v3) {
     int i;
-    double cross_product = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-    if (cross_product <= 0) {
-        return 0; // not a convex corner
-    }
-    for (i = 0; i < num_points; i++) {
-        if ((points[i].x == a.x && points[i].y == a.y) || (points[i].x == b.x && points[i].y == b.y) || (points[i].x == c.x && points[i].y == c.y)) {
-            continue;
-        }
-        double cross_product1 = (b.x - a.x) * (points[i].y - a.y) - (b.y - a.y) * (points[i].x - a.x);
-        double cross_product2 = (c.x - b.x) * (points[i].y - b.y) - (c.y - b.y) * (points[i].x - b.x);
-        double cross_product3 = (a.x - c.x) * (points[i].y - c.y) - (a.y - c.y) * (points[i].x - c.x);
-        if (cross_product1 >= 0 && cross_product2 >= 0 && cross_product3 >= 0) {
-            return 0; // point inside triangle
+    for (i = 0; i < numVertices; i++) {
+        if (i != v1 && i != v2 && i != v3) {
+            float cross1 = (polygon[v2].x - polygon[v1].x) * (polygon[i].y - polygon[v2].y) -
+                           (polygon[v2].y - polygon[v1].y) * (polygon[i].x - polygon[v2].x);
+            float cross2 = (polygon[v3].x - polygon[v2].x) * (polygon[i].y - polygon[v3].y) -
+                           (polygon[v3].y - polygon[v2].y) * (polygon[i].x - polygon[v3].x);
+            float cross3 = (polygon[v1].x - polygon[v3].x) * (polygon[i].y - polygon[v1].y) -
+                           (polygon[v1].y - polygon[v3].y) * (polygon[i].x - polygon[v1].x);
+            
+            if (cross1 >= 0 && cross2 >= 0 && cross3 >= 0)
+                return 0;
         }
     }
+    
     return 1;
 }
 
-void triangulate(struct point *points, int num_points, struct triangle **triangles, int *num_triangles) {
-    int i, j, k;
-    int *indices = malloc(num_points * sizeof(int));
-    for (i = 0; i < num_points; i++) {
-        indices[i] = i;
-    }
-    *triangles = NULL;
-    *num_triangles = 0;
-    int orientation = 0; // 0 for counterclockwise, 1 for clockwise
-    double cross_product = 0;
-    for (i = 0; i < num_points; i++) {
-        j = (i + 1) % num_points;
-        k = (i + 2) % num_points;
-        cross_product = (points[j].x - points[i].x) * (points[k].y - points[i].y) - (points[j].y - points[i].y) * (points[k].x - points[i].x);
-        if (cross_product != 0) {
-            orientation = (cross_product > 0) ? 0 : 1;
-            break;
+Triangle* earClippingTriangulation(Point* polygon, int numVertices) {
+    int i, v1, v2, v3;
+    int numTriangles = numVertices - 2;
+    Triangle* triangles = (Triangle*)malloc(numTriangles * sizeof(Triangle));
+    
+    v1 = 0;
+    v2 = 1;
+    v3 = 2;
+    Triangle triangle = {v1, v2, v3};
+    triangles[0] = triangle;
+    
+    for (i = 3; i < numVertices; i++) {
+        v2 = v3;
+        v3 = i;
+        
+        if (isEarTip(polygon, numVertices, v1, v2, v3)) {
+            Triangle triangle = {v1, v2, v3};
+            triangles[i - 2] = triangle;
+        } else {
+            v1 = v2;
+            v2 = v3;
+            v3++;
         }
     }
-    if (orientation == 1) {
-        for (i = 0; i < num_points; i++) {
-            j = (i + 1) % num_points;
-            indices[i] = (num_points - 1) - i;
-        }
+    
+    return triangles;
+}
+
+void printTriangles(Triangle* triangles, int numTriangles) {
+    printf("Triangulation:\n");
+    for (int i = 0; i < numTriangles; i++) {
+        printf("Triangle %d: %d %d %d\n", i+1, triangles[i].v1, triangles[i].v2, triangles[i].v3);
     }
-    while (num_points >= 3) {
-        for (i = 0; i < num_points; i++) {
-            j = (i + 1) % num_points;
-            k = (i + 2) % num_points;
-            if (is_ear(points[indices[i]], points[indices[j]], points[indices[k]], points, num_points)) {
-                struct triangle t = {points[indices[i]], points[indices[j]], points[indices[k]]};
-                (*triangles) = realloc(*triangles, (*num_triangles + 1) * sizeof(struct triangle));
-                (*triangles)[*num_triangles] = t;
-                (*num_triangles)++;
-                for (j = i + 1; j < num_points - 1; j++) {
-                    indices[j] = indices[j + 1];
-                }
-                num_points--;
-                break;
-            }
-        }
-    }
-    free(indices);
 }
 
 int main() {
-    struct point points[] = {{1, 4}, {1, 20}, {15, 20}, {15, 4}};
-    int num_points = sizeof(points) / sizeof(struct point);
-    struct triangle *triangles;
-    int num_triangles;
-    triangulate(points, num_points, &triangles, &num_triangles);
-    printf("%d triangles:\n", num_triangles);
-    int i;
-    for (i = 0; i < num_triangles; i++) {
-        printf("Triangle %d: (%f, %f), (%f, %f), (%f, %f)\n", i+1, triangles[i].a.x, triangles[i].a.y, triangles[i].b.x, triangles[i].b.y, triangles[i].c.x, triangles[i].c.y);
-    }
+    // Define the polygon vertices in counterclockwise order
+    Point polygon[] = {{1, 4}, {1, 20}, {15, 20}, {15, 4}};
+    int numVertices = sizeof(polygon) / sizeof(Point);
+    
+    // Triangulate the polygon
+    Triangle* triangles = earClippingTriangulation(polygon, numVertices);
+    int numTriangles = numVertices - 2;
+    
+    // Print the triangles
+    printTriangles(triangles, numTriangles);
+    
+    // Cleanup
     free(triangles);
+    
     return 0;
 }
